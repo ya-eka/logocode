2af422b892f712596979b925b3443a7390653707
Ягодарова Екатерина Валерьевна <ekaterina.yagodarova@office.moneta.ru>
2024-08-20 15:20:00 +0300

    Add lesson5

---
+++ lesson5/task3.js
@@ -0,0 +1,9 @@
+const score = 90;
+
+if (score > 70) {
+    console.log('"Хорошая работа!"');
+    if (score > 90) {
+    console.log('"Отличная работа!"');
+}} else {
+    console.log('"Попробуйте еще раз"');
+}
---
+++ lesson5/task5.js
@@ -0,0 +1,10 @@
+let num1 = 15, 
+    num2 = 15;
+
+if (num1 > num2) {
+    console.log('num1 больше num2');
+} else if (num1 == num2) {
+    console.log('num1 и num2 равны');
+} else {
+    console.log('num2 больше num1');
+}
