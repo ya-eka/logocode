2fd75cea966c35d5c48bff153d851b456a9a70ef
Ягодарова Екатерина Валерьевна <ekaterina.yagodarova@office.moneta.ru>
2024-08-26 15:18:28 +0300

    Changed the file name from 5 to 6

---
+++ lesson6/task1.js
@@ -0,0 +1,21 @@
+    "42" == 42  // true
+
+    "0" == 0    // true
+
+    "0" == false  //  true
+
+    "true" == true // false
+
+    true == (1 == "1") // false
+
+
+    "42" === 42 // false
+
+    "0" === 0   // false
+
+    "0" === false  // false
+
+    "true" === true // false
+
+    true === (1 === "1") // false
+
---
+++ lesson6/task4.js
@@ -0,0 +1,17 @@
+let number = 6;
+
+// number % 2 == 0
+//     ? (
+//         console.log('Значение ', number, ' - чётное число')
+//     )
+//     : (
+//         console.log('Значение ', number, ' - нечётное число')
+//     );
+
+number % 2 === 0
+    ? (
+        console.log(number, ': нечётное число')
+    )
+    : (
+        console.log(number, ': нечётное число')
+    )
