e05419caa90f51f5c13730c700b2d0f9ce353660
Ягодарова Екатерина Валерьевна <ekaterina.yagodarova@office.moneta.ru>
2024-08-26 11:28:31 +0300

    Fixed task6

--- lesson5/task4.js
+++ lesson5/task4.js
@@ -1,9 +1,11 @@
 const number = 6;
 
-number % 2 == 0
-    ? (
-        console.log('Значение ', number, ' - чётное число')
-    )
-    : (
-        console.log('Значение ', number, ' - нечётное число')
-    );
+// number % 2 == 0
+//     ? (
+//         console.log('Значение ', number, ' - чётное число')
+//     )
+//     : (
+//         console.log('Значение ', number, ' - нечётное число')
+//     );
+
+console.log(${number} - ${number} % 2 == 0? 'чётное' : 'нечётное' + 'число');
