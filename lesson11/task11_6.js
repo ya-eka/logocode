772e25f063c7c49bae03a4cb84e89c3433981e3e
Ягодарова Екатерина Валерьевна <ekaterina.yagodarova@office.moneta.ru>
2024-09-13 12:06:55 +0300

    Fix lesson11 / task6

--- lesson11/task11_6.js
+++ lesson11/task11_6.js
@@ -7,14 +7,45 @@
 //
 // check();
 
+// let squareNumber = (num) => num ** 2;
+
+// function check(x) {
+//     if (18 < x < 50) {  //18 < 5 = false, false < 50 = 0 < 50 = true
+//         console.log(squareNumber(x))
+//      } else {
+//         throw new Error('exception')
+//      }  
+// };
+// check(5);
+
+
+// let squareNumber = (num) => num ** 2;
+
+// function check(x) {
+//     if ((18 < x) & (x < 50)) {
+//         console.log(squareNumber(x))
+//      } else {
+//         throw new Error('exception')
+//      }  
+// };
+
+// check(19);
+
 let squareNumber = (num) => num ** 2;
 
 function check(x) {
-    if (18 < x < 50) {
-        console.log(squareNumber(x))
-     } else {
-        throw new Error('exception')
-     }  
+   try {
+      if ((18 < x) & (x < 50)) {
+         console.log(squareNumber(x))
+      }
+      else {
+         throw new Error('exception')
+      }
+   } catch (error) {
+      console.log(`Было выброшено исключение, так как число ${x} не входит в необходимый диапазон.`)
+      //throw new Error('exception')
+   }
 };
 
 check(5);
+
